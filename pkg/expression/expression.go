a436c27464d11d7201aef9226695c115ea406f8d
Ishan Arya <ishanarya0@gmail.com>
2024-03-07 11:51:19 +0530

    feat: move attribute, operator and value to root

--- pkg/expression/expression.go
+++ pkg/expression/expression.go
@@ -7,30 +7,24 @@ import (
 )
 
 type Expression struct {
-	Comparison Comparison `yaml:"comparison" mapstructure:"comparison"`
-}
-
-type Comparison struct {
-	Key      any    `yaml:"key" mapstructure:"key"`
-	Operator string `yaml:"operator" mapstructure:"operator"`
-	Value    any    `yaml:"value" mapstructure:"value"`
+	Attribute any    `yaml:"attribute" mapstructure:"attribute"`
+	Operator  string `yaml:"operator" mapstructure:"operator"`
+	Value     any    `yaml:"value" mapstructure:"value"`
 }
 
 func (e Expression) Evaluate() (bool, error) {
-	if !reflect.ValueOf(e.Comparison).IsZero() {
-		output := false
-		var err error
-		switch e.Comparison.Operator {
-		case "==":
-			output = reflect.DeepEqual(e.Comparison.Key, e.Comparison.Value)
-			err = nil
-		default:
-			err = errors.New(fmt.Sprintf("unsupported comparison operator %s", e.Comparison.Operator))
-		}
+	output := false
+	var err error
 
-		return output, err
+	switch e.Operator {
+	case "==":
+		output = reflect.DeepEqual(e.Attribute, e.Value)
+		err = nil
+	default:
+		err = errors.New(fmt.Sprintf("unsupported operator %s", e.Operator))
 	}
-	return false, errors.New("no supported operators configured")
+
+	return output, err
 }
 
 func (e Expression) IsEmpty() bool {
