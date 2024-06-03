5d7064d1160b6a1aff55a6fe4f61d8c2a70da93c
Ishan Arya <ishanarya0@gmail.com>
2024-06-03 20:25:44 +0530

    fix: sql commands closing

--- internal/store/postgres/migrations/20240603061854_rename_activities_table.up.sql
+++ internal/store/postgres/migrations/20240603061854_rename_activities_table.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE public.activities RENAME to old_activities;
 
-ALTER INDEX IF EXISTS activity_timestamp_index RENAME TO old_activity_timestamp_index
-ALTER INDEX IF EXISTS activity_action_index RENAME TO old_activity_action_index
-ALTER INDEX IF EXISTS activity_actor_index RENAME TO old_activity_actor_index
-ALTER INDEX IF EXISTS activity_data_index RENAME TO old_activity_data_index
-ALTER INDEX IF EXISTS activity_metadata_index RENAME TO old_activity_metadata_index
+ALTER INDEX IF EXISTS activity_timestamp_index RENAME TO old_activity_timestamp_index;
+ALTER INDEX IF EXISTS activity_action_index RENAME TO old_activity_action_index;
+ALTER INDEX IF EXISTS activity_actor_index RENAME TO old_activity_actor_index;
+ALTER INDEX IF EXISTS activity_data_index RENAME TO old_activity_data_index;
+ALTER INDEX IF EXISTS activity_metadata_index RENAME TO old_activity_metadata_index;
