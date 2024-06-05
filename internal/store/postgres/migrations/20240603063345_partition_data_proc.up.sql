ba2cfd4a26144ccf7acd5f0a0f4d12d9d2853baa
Ishan Arya <ishanarya0@gmail.com>
2024-06-05 18:24:11 +0530

    fix: call partition procedure in a DO statement

--- internal/store/postgres/migrations/20240603063345_partition_data_proc.up.sql
+++ internal/store/postgres/migrations/20240603063345_partition_data_proc.up.sql
@@ -1,5 +1,6 @@
-BEGIN TRANSACTION;
-
-  CALL partman.partition_data_proc('public.activities', p_batch := 100, p_source_table := 'public.old_activities');
-
-COMMIT;
+DO
+$$
+BEGIN
+    CALL partman.partition_data_proc('public.activities', p_batch := 100, p_source_table := 'public.old_activities');
+END;
+$$;
