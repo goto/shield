c6035aada84abc30d7e1ea737b4d3a0a5e55bf07
Ishan Arya <ishanarya0@gmail.com>
2024-06-05 18:54:48 +0530

    fix: call partition procedure w/o any block

--- internal/store/postgres/migrations/20240603063345_partition_data_proc.up.sql
+++ internal/store/postgres/migrations/20240603063345_partition_data_proc.up.sql
@@ -1,6 +1,1 @@
-DO
-$$
-BEGIN
-    CALL partman.partition_data_proc('public.activities', p_batch := 100, p_source_table := 'public.old_activities');
-END;
-$$;
+CALL partman.partition_data_proc('public.activities', p_batch := 100, p_source_table := 'public.old_activities');
