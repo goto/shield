dd54d61dd0d16b057539ea98a31fd77a2a3b801d
Ishan Arya <ishanarya0@gmail.com>
2024-06-05 19:05:11 +0530

    fix: call partition procedure w/o a txn block (#67)
    * fix: call partition procedure in a DO statement
    
    * fix: call partition procedure w/o any block

--- internal/store/postgres/migrations/20240603063345_partition_data_proc.up.sql
+++ internal/store/postgres/migrations/20240603063345_partition_data_proc.up.sql
@@ -1,5 +1,1 @@
-BEGIN TRANSACTION;
-
-  CALL partman.partition_data_proc('public.activities', p_batch := 100, p_source_table := 'public.old_activities');
-
-COMMIT;
+CALL partman.partition_data_proc('public.activities', p_batch := 100, p_source_table := 'public.old_activities');
