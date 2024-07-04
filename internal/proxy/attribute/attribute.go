9c4df989be44fa36bd0a3328091cf82e06579b1d
Femi Novia Lina <feminovi@gmail.com>
2024-07-04 17:09:09 +0700

    fix: delimiter

--- internal/proxy/attribute/attribute.go
+++ internal/proxy/attribute/attribute.go
@@ -32,8 +32,8 @@ type Attribute struct {
 }
 
 func ComposeAttribute(attribute string, attrs map[string]interface{}) string {
-	if strings.Contains(attribute, "{") {
-		template := fasttemplate.New(attribute, "{", "}")
+	if strings.Contains(attribute, "${") {
+		template := fasttemplate.New(attribute, "${", "}")
 		return template.ExecuteString(attrs)
 	}
 	return attribute
